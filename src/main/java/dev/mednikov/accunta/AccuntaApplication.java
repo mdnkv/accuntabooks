f40ed1610a9af83d466f8dfa0741e918a4eac15d
Iurii Mednikov <yurimednikov@proton.me>
2025-04-18 10:05:04 +0200

    Initial commit

---
+++ src/main/java/dev/mednikov/accunta/AccuntaApplication.java
@@ -0,0 +1,13 @@
+package dev.mednikov.accunta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccuntaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccuntaApplication.class, args);
+	}
+
+}
